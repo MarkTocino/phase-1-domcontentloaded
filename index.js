2a5c1f81aaedef915f8c9035f2308fc0ff052069
MarkTocino <Mark_tocino@yahoo.com>
2023-06-17 18:00:19 -0400

    Completed domcontentloaded lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+addEventListener('DOMContentLoaded', () => {
+
+const text = document.querySelector('#text')
+const theText = "This is really cool!"
+text.textContent = theText
+});
